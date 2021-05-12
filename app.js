8bb831cbeaedc0c120b77929130e603279a9ef93
Pierdomenico Apruzzese <pieroapruzzese@gmail.com>
2021-05-12 19:13:20 +0200

    load

--- app.js
+++ app.js
@@ -5,12 +5,23 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import NavBar from "./components/NavBar";
 //import Home from "./components/Home";
+import LatestRelease from "./components/latestrelease"
+import Fantasy from "../data/fantasy.json";
+import History from "../data/history.json";
+import Horror from "../data/horror.json";
+import Romance from "../data/romance.json";
+import Scifi from "../data/scifi.json";
 
 class App extends React.Component {
   render() {
     return (
       <>
         <NavBar title="Strive Library" />
+      
+
+        <LatestRelease props= {Fantasy}/>
+
+        
         
       </>
     );
